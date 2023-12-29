68e650625dd8d5c38ba4b051563ded12e93c8ee7
VaibhavRanga <vaibhavranga2@gmail.com>
2023-12-30 02:57:22 +0530

    first commit

---
+++ lib/dice_roller.dart
@@ -0,0 +1,38 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+class DiceRoller extends StatefulWidget {
+  const DiceRoller({super.key});
+
+  @override
+  State<DiceRoller> createState() => _DiceRollerState();
+}
+
+class _DiceRollerState extends State<DiceRoller> {
+  var currentDiceImage = 'assets/images/dice-${Random().nextInt(6) + 1}.png';
+  rollDice() {
+    setState(() {
+      currentDiceImage = 'assets/images/dice-${Random().nextInt(6) + 1}.png';
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Image.asset(
+          currentDiceImage,
+          width: 200,
+        ),
+        const SizedBox(height: 20),
+        FilledButton.tonal(
+          onPressed: rollDice,
+          child: const Text('Roll Dice'),
+        ),
+      ],
+    );
+  }
+}
